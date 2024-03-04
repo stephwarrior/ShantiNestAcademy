8185e85024579109d438949f31872727b931d8b5
Stephanie Guerrier <e1574836@cmaisonneuve.qc.ca>
2024-03-03 20:42:43 -0500

    Programmation page services (visuel)  boucle d'articles + affichage boîte modale

--- src/pages/PageServices.vue
+++ src/pages/PageServices.vue
@@ -1,11 +1,29 @@
 <template>
   <main>
     <v-breadcrumbs :items="items"></v-breadcrumbs>
-    <div class="blocService" v-for="(service, index) in services" :key="index">
-      <h1>{{ service.name }}</h1>
-      <img :src="service.img" alt="pas image" />
-      <p>{{ service.description }}</p>
-    </div>
+    <v-container>
+      <v-row v-for="(service, index) in services" :key="index"
+        ><v-col cols="12" md="4">
+          <v-img :src="service.img" aspect-ratio="1.4" class="grey lighten-2"></v-img>
+        </v-col>
+        <v-col cols="12" md="8">
+          <h1>{{ service.name }}</h1>
+          <p>{{ service.descriptionCourt }}</p>
+          <v-btn color="primary" @click="ouvreDialogue(service)">Voir plus</v-btn>
+        </v-col>
+      </v-row>
+
+      <v-dialog v-model="dialog" persistent max-width="600px">
+        <v-card>
+          <v-card-title>{{ itemSelectionner.name }}</v-card-title>
+          <v-card-text>{{ itemSelectionner.descriptionLong }}</v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="green darken-1" text @click="dialog = false">Fermer</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-container>
   </main>
 </template>
 <script>
@@ -16,16 +34,29 @@ export default {
         {
           img: "https://unpasverssoi.ca/fr/wp-content/uploads/2018/04/salle-cours-yoga.jpg",
           name: "Location des salles",
-          description:
+          descriptionCourt:
+            "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores.",
+          descriptionLong:
             "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat",
         },
         {
           img: "https://sautoformer.fr/wp-content/uploads/2019/04/formation-professeur-yoga.jpg",
           name: "Formation des cours",
-          description:
-            "Lorem ipsumAt vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat",
+          descriptionCourt:
+            "Lorem ipsumAt vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium.",
+          descriptionLong:
+            "voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat",
+        },
+        {
+          img: "https://sautoformer.fr/wp-content/uploads/2019/04/formation-professeur-yoga.jpg",
+          name: "Carrière",
+          descriptionCourt: "Vero eos et accusamus et iusto odio dignissimos ducimus qui blanditi praesentium.",
+          descriptionLong:
+            "Vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et qua non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat",
         },
       ],
+      dialog: false,
+      itemSelectionner: null,
       items: [
         {
           title: "Accueil",
@@ -39,9 +70,19 @@ export default {
       ],
     };
   },
+  methods: {
+    ouvreDialogue(item) {
+      this.itemSelectionner = item;
+      this.dialog = true;
+    },
+  },
 };
 </script>
+
 <style scoped>
+main {
+  height: auto;
+}
 .blocService {
   display: flex;
   flex-direction: column;
