17c6a2e99ca5c1528d7d46567448b92e717048e8
Stephanie Guerrier <e1574836@cmaisonneuve.qc.ca>
2024-03-04 02:17:38 -0500

    Page boutique: positionnement des bloc d'items

--- src/pages/PageBoutique.vue
+++ src/pages/PageBoutique.vue
@@ -1,13 +1,19 @@
 <template>
   <main>
-    <div class="test"><RouterLink to="/">Accueil</RouterLink>/ Boutique</div>
-    <div>{{ nombre }} items</div>
-    <div v-for="(item, index) in items" :key="index" class="blocItems">
-      <img alt="pas image" />
-      <p>{{ item.nom }}</p>
-      <p>{{ item.prix }}</p>
-      <button>Ajouter au panier</button>
-    </div>
+    <v-breadcrumbs :items="items"></v-breadcrumbs>
+    <v-container class="d-flex justify-space-evenly">
+      <div>{{ lesItems.length }} items</div>
+      <div>filtrer par</div>
+    </v-container>
+
+    <v-container class="test">
+      <v-card v-for="(item, index) in lesItems" :key="index" class="blocItems">
+        <v-img :src="item.img" alt="pas image">zone image</v-img>
+        <p>{{ item.nom }}</p>
+        <p>${{ item.prix }}.00 CAD</p>
+        <v-btn>Ajouter au panier</v-btn>
+      </v-card></v-container
+    >
   </main>
 </template>
 <script>
@@ -15,11 +21,30 @@ export default {
   name: "PageBoutique",
   data() {
     return {
-      nombre: 0,
+      lesItems: [
+        {
+          nom: "Goblet SNA",
+          prix: 50,
+          img: "",
+        },
+        { nom: "Chandail SNA", prix: 10, img: "" },
+        { nom: "Bouteille SNA", prix: 15, img: "" },
+        { nom: "Tapis SNA", prix: 20, img: "" },
+        { nom: "Tasse SNA", prix: 10, img: "" },
+        { nom: "Sac SNA", prix: 25, img: "" },
+        { nom: "Bandeau SNA", prix: 5, img: "" },
+        { nom: "Bouteille SNA", prix: 15, img: "" },
+      ],
       items: [
-        { nom: "Goblet SNA", prix: 50 },
-        { nom: "Chandail SNA", prix: 10 },
-        { nom: "Bouteille SNA", prix: 15 },
+        {
+          title: "Accueil",
+          desactive: false,
+          href: "/",
+        },
+        {
+          title: "Boutique",
+          desactive: true,
+        },
       ],
     };
   },
@@ -30,13 +55,11 @@ export default {
 .blocItems {
   display: flex;
   flex-direction: column;
-  align-items: center;
   justify-content: center;
-  border: 1px solid black;
-  margin: 10px;
-  padding: 10px;
-  width: 200px;
-  height: 200px;
-  background-color: #f0f0f0;
+  align-items: center;
+}
+.test {
+  display: flex;
+  justify-content: space-around;
 }
 </style>
